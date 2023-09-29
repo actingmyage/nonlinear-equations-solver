9755f0445b266838ccf84459ba17f08ebc4afbf9
Chesnokov Arkady <arkady-chesnokof@yandex.ru>
2023-09-29 14:36:50 +0300

    unfinished commit

---
+++ system_solver.py
@@ -0,0 +1,33 @@
+import numpy as np
+from scipy.optimize import minimize_scalar
+
+
+def equation_1(x1, x2):
+    return 0.1 * x1 ** 2 + x1 + 0.2 * x2 ** 2 - 0.3
+
+
+def equation_2(x1, x2):
+    return 0.2 * x1 ** 2 + x2 + 0.1 * x1 * x2 - 0.7
+
+
+def phi_1(x1, x2):
+    return 0.3 - 0.1 * x1 ** 2 - 0.2 * x2 ** 2
+
+
+def phi_2(x1, x2):
+    return 0.7 - 0.2 * x1 ** 2 - 0.1 * x1 * x2
+
+
+SYSTEMS = [
+    [[equation_1, equation_2], [phi_1, phi_2]]
+]
+
+
+def system_simple_iteration_method(sys, epsilon):
+    print("determine the square in which the equation will be located")
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    
+
