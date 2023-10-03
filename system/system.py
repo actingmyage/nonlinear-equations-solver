3516398b2991cd6202cf0b499ff000182e532af0
Chesnokov Arkady <arkady-chesnokof@yandex.ru>
2023-10-03 11:46:28 +0300

    add system_1 and system_2 [prev commit was empty]

--- system/system.py
+++ system/system.py
@@ -1,11 +1,12 @@
-def equation_1(x, y):
-    return x ** 2 + y ** 2 - 1
+from sympy import symbols, diff
 
+x, y = symbols('x y')
 
-def equation_2(x, y):
-    return x ** 2 - y - 0.5
+equation_1 = x ** 2 + y ** 2 - 1
 
+equation_2 = x ** 2 - y - 0.5
 
-def equation_3(x, y):
-    return x - y ** 2
+equation_3 = x - y ** 2
 
+SYSTEM_1 = [equation_1, equation_2]
+SYSTEM_2 = [equation_1, equation_3]
